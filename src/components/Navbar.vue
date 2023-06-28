ee5d492e274649fe6c4e46ec9ae5cbba82be0f46
Dawn Tran <kdoantran@gmail.com>
2023-06-28 16:28:28 -0500

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,54 @@
+     
+<template>
+  <MDBNavbar expand="lg" light bg="light" container>
+    <MDBNavbarBrand href="#">Brand</MDBNavbarBrand>
+    <MDBNavbarToggler
+      @click="collapse1 = !collapse1"
+      target="#navbarSupportedContent"
+    ></MDBNavbarToggler>
+    <MDBCollapse v-model="collapse1" id="navbarSupportedContent">
+      <MDBNavbarNav class="mb-2 mb-lg-0">
+        <MDBNavbarItem to="#" active>
+          Home
+        </MDBNavbarItem>
+        <MDBNavbarItem href="#">
+          Bio
+        </MDBNavbarItem>
+        <MDBNavbarItem>
+          <!-- Navbar dropdown -->
+          <MDBDropdown class="nav-item" v-model="dropdown1">
+            <MDBDropdownToggle
+              tag="a"
+              class="nav-link"
+              @click="dropdown1 = !dropdown1"
+              >Courses</MDBDropdownToggle
+            >
+            <MDBDropdownMenu aria-labelledby="dropdownMenuButton">
+              <MDBDropdownItem href="#">Algebra 1</MDBDropdownItem>
+              <MDBDropdownItem href="#">Algebra 2</MDBDropdownItem>
+              <MDBDropdownItem href="#"
+                >Something else here</MDBDropdownItem
+              >
+            </MDBDropdownMenu>
+          </MDBDropdown>
+      </MDBNavbarNav>
+      <!-- Search form -->
+      <form class="d-flex input-group w-auto">
+        <input
+          type="search"
+          class="form-control"
+          placeholder="Search "
+          aria-label="Search"
+        />
+        <MDBBtn outline="primary">
+          Search
+        </MDBBtn>
+      </form>
+    </MDBCollapse>
+  </MDBNavbar>
+</template>
+
+
 <template>
   <nav>
     <ul>
@@ -18,4 +69,4 @@ export default {
 <style>
 /* Use top-level NavBar style */
 
-</style>     
+</style>

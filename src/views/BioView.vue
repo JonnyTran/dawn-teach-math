d18f315bffadb1025431347fa5eb981a3fbc7ebd
Dawn Tran <kdoantran@gmail.com>
2023-06-30 02:46:39 -0500

    contact & bio

--- src/views/BioView.vue
+++ src/views/BioView.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <h1>Bio</h1>
+    <portrait>
+    <img src="https://i.imgur.com/rTeadEW.png" alt="teacher" width="250" height="300">
+    </portrait>
+  </div>
+  <div>
+    <bio>
+    <biotitle><h1>Bio</h1></biotitle>
     <p>Name: {{ bio.name }}</p>
     <p>Qualifications: {{ bio.qualifications }}</p>
     <p>Teaching Experience: {{ bio.teachingExperience }}</p>
-    <p>Introduction: {{ bio.introduction }}</p>
+    <p>About me: {{ bio.about }}</p>
+    </bio>
+  </div>
+  <div>
+    <quote>
+    <h1>“The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education.”</h1>
+    <author><h2>-Martin Luther King, Jr.</h2></author>
+    </quote>
   </div>
 </template>
 
@@ -14,16 +27,63 @@ export default {
   data() {
     return {
       bio: {
-        name: 'John Doe',
-        qualifications: 'M.Sc. in Mathematics',
-        teachingExperience: '10 years',
-        introduction: 'I am passionate about teaching mathematics and helping students succeed.',
+        name: 'Dawn Tran',
+        qualifications: 'B.S. in Mathematics at the University of Texas at Austin (2021)',
+        teachingExperience: '2 years',
+        about: 'I am originally from Vietnam and moved to the United States when I was very young. That said, please do not ask me to teach you any Vietnamese. I can teach math way better!',
       },
     };
   },
-};
+}; 
 </script>
 
 <style>
-/* Add your custom styles here */
+portrait {
+  position: absolute;
+  top: 140px;
+  left: 140px;
+  background: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #000000;
+  padding: 10px;
+  border-bottom: 6px solid #02150aa7;
+  border-right: 4px solid #02150aa7;
+}
+
+biotitle {
+  font-size: 1.8rem;
+  font-weight: 450;
+  color: #02150aa7;
+}
+
+bio {
+  position: absolute;
+  top: 160px;
+  left: 500px;
+  width: 590px;
+  background: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #000000;
+  padding: 10px;
+  border-bottom: 6px solid #02150aa7;
+  border-right: 4px solid #02150aa7;
+  line-height: 2;
+}
+
+quote {
+  position: absolute;
+  align-self: center;
+  font-size: larger;
+  width: 700px;
+  top: 500px;
+  left: 500px;
+  font-style: oblique;
+  line-height: 2;
+}
+
+author {
+  line-height: 3;
+}
+
 </style>
+
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
+    <contact>
     <h1>Contact</h1>
+    </contact>
     <form @submit.prevent="submitForm">
+      <name>
       <label for="name">Name:</label>
-      <input type="text" id="name" v-model="form.name" required>
+      <input type="text" id="name" v-model="form.name" required> 
+      </name>
+      <email>
       <label for="email">Email:</label>
       <input type="email" id="email" v-model="form.email" required>
+      </email>
+      <message>
       <label for="message">Message:</label>
       <textarea id="message" v-model="form.message" required></textarea>
+      </message>
       <button type="submit">Submit</button>
     </form>
   </div>
@@ -35,5 +43,99 @@ export default {
 </script>
 
 <style>
-/* Add your custom styles here */
+contact {
+  position: absolute;
+  top: 15.5%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.8rem;
+  font-weight: 450;
+  color: #02150aa7;
+}
+
+form {
+  position: absolute;
+  top: 51%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height:500px;
+  width: 450px;
+  background: #ffffff;
+  border-radius: 8px;
+  border: 1px solid #000000;
+  padding: 10px;
+  border-bottom: 6px solid #02150aa7;
+  border-right: 4px solid #02150aa7;
+}
+
+form label {
+  margin: 10px;
+  font-size: 1.1rem;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+form name, textarea {
+  display: flex;
+  align-self: center;
+  flex-direction: column;
+  height: 90px;
+  width: 90%;
+  margin-bottom: 10px;
+}
+
+form email, textarea {
+  display: flex;
+  align-self: center;  align-self: center;
+  flex-direction: column;
+  height: 90px;
+  width: 90%;
+  margin-bottom: 10px;
+}
+
+form message, textarea {
+  display: flex;
+  align-self: center;
+  flex-direction: column;
+  height: 200px;
+  width: 95%;
+  border-radius: 6px;
+  margin-bottom: 10px;
+}
+
+form button {
+  align-self: center;
+  width: 50%;
+  height: 50px;
+  border: 2.5px solid rgba(37, 89, 211, 0.738);
+  border-radius: 8px;
+  color: rgb(37, 89, 211);
+  font-size: 1.1rem;
+  font-weight: bold;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  margin-bottom: 10px;
+}
+
+form button:hover {
+  background: rgb(37, 89, 211);
+  color: #ffffff;
+  border: 2px solid #ffffff;
+  font-size: 1.12rem;
+}
+
+form button:focus {
+  outline: none;
+}
+
+form button:active {
+  transform: scale(0.98);
+}
+
+
+
+
 </style>

dacb85016c89e810a57bfb1737656bd41ddd5766
Jonny Tran <nhat.tran@mavs.uta.edu>
2023-07-07 00:27:00 -0500

    edited HomeView

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,7 +12,7 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink, Input, Button, Toggle }
     <template #default="{isShowMenu}">
       <NavbarCollapse :isShowMenu="isShowMenu">
         <!-- Set isActive to NavbarLink when in the currentRoute -->
-        <NavbarLink :isActive="true">
+        <NavbarLink>
           <router-link to="/">Home</router-link>
         </NavbarLink>
         <NavbarLink>
@@ -26,7 +26,7 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink, Input, Button, Toggle }
         </NavbarLink>
       </NavbarCollapse>
 
-      <Input size="lg" placeholder="">
+      <!-- <Input size="lg" placeholder="">
         <template #prefix>
           <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
@@ -35,7 +35,7 @@ import { Navbar, NavbarLogo, NavbarCollapse, NavbarLink, Input, Button, Toggle }
         <template #suffix>
           <Button size="md">Search</Button>
         </template>
-      </Input>
+      </Input> -->
     </template>
   </Navbar>
   <!-- Add a second level navbar for Class or Unit here -->
@@ -59,9 +59,9 @@ export default {
 
 <style>
 
-/* Style the NavBar */ 
+/* Set text to bold on active */ 
 .is-active {
-  background-color: gray;
+  font-weight: bold;
 }
 
 /* nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,52 @@
+<script setup>
+import { ref } from 'vue'
+import { Tabs, Tab } from 'flowbite-vue'
+const activeTab = ref('first')
+</script>
+
 <template>
-  <div>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          Hello, this is Ms. Tran website!
+        </h1>
+        <p class="max-w-2xl mb-6 font-bold italic text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+          The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education.
+          <br/>
+          -Martin Luther King, Jr.
+        </p>
+        <p></p>
+        <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          Go to Courses
+        </a> 
+      </div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <img src="../../src/assets/hero-section-splash.jpg" alt="mockup">
+      </div>                
+    </div>
+  </section>
+  <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700">
+
+  <section class="flex justify-center">
+    <tabs v-model="activeTab" class="p-5"> <!-- class appends to content DIV for all tabs -->
+      <tab name="first" title="First">
+        Lorem...
+      </tab>
+      <tab name="second" title="Second">
+        Lorem...
+      </tab>
+      <tab name="third" title="Third">
+        Lorem...
+      </tab>
+      <tab name="fourth" title="Fourth">
+        Lorem...
+      </tab>
+    </tabs>
+  </section>
+
+
+  <!-- <div>
     <h1 class="font-bold">Welcome to the Teacher Profile Website</h1>
     <h2>Courses</h2>
     <h3>
@@ -8,7 +55,7 @@
         <p>{{ course.description }}</p>
       </text>
     </h3>
-  </div>
+  </div> -->
 </template>
 
 <script>

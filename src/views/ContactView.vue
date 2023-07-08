c94a538d5100266e396fb725c29ec646b13c74cf
Dawn Tran <kdoantran@gmail.com>
2023-07-08 00:58:16 -0500

    contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -16,14 +16,24 @@
       <email>
       <label for="email">Email:</label>
       <input type="email" name="email" v-model="form.email" required>
+      <span v-if="emailError">{{ emailError }}</span>
       </email>
       <subject>
       <label for="subject">Subject:</label>
-      <input type="text" name="subject " v-model="form.subject" required>
+      <select name="subject " v-model="selectedSubject" class="bg-gray-50 border border-gray-300 text-gray-900 text-md lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+        <option value="" disabled selected>Choose a subject</option>
+        <option value="GR">Grades</option>
+        <option value="AS">Assignments</option>
+        <option value="RE">Retest/Missed test or quiz</option>
+        <option value="TU">Tutoring</option>
+        <option value="AT">Attendance</option>
+        <option value="PC">Parent contact</option>
+        <option value="OT">Other</option>
+      </select>
       </subject>
       <message>
       <label for="message">Message:</label>
-      <textarea name="message" v-model="form.message" required></textarea>
+      <textarea name="message" v-model="form.message" class="input-style" :placeholder="getPlaceholder()" required></textarea>
       </message>
       <button type="submit">Submit</button>
     </form>
@@ -31,21 +41,48 @@
 </template>
 
 <script>
-import Dropdown from 'vue-simple-search-dropdown';
+
 const WEB3FORMS_ACCESS_KEY = "15362813-1203-485a-b258-4227d01d0bd6";
+
 export default {
   name: 'ContactPage',
   data() {
     return {
+      selectedSubject: '',
       form: {
         name: '',
         email: '',
-        subject:'',
         message: '',
       },
+      emailError: '',
     };
   },
   methods: {
+      getPlaceholder() {
+        switch (this.selectedSubject) {
+          case "GR":
+            return 'e.g., When will grades be posted for...\nHow can I make up this grade...\nPlease update this grade...';
+          case "AS":
+            return 'e.g., Did you receive my work for...\nI need to make up this assignment...\nI won\'t be able to turn in this assignment... ';
+          case "RE":
+            return 'Testing Hours:\n- After school Tues-Thu 4:30-5:45 PM\n- Morning by teacher availability\n- Flex self-schedule on Edficiency\nMust be completed in one continuous session';
+          case "TU":
+            return 'Tutoring Hours:\n- After school Mon/Wed 4:20-5:45 PM\n- Morning by teacher availability\n- Flex self-schedule on Edficiency';
+          case "AT":
+            return 'e.g., I will be absent during...\nPlease fix my attendance for...\nWhat will I miss on this day...';
+          case "PC":
+            return 'e.g., Hello, I am the parent of...\nI would like to schedule a call or meeting...\nPlease contact me at...';
+          case "OT":
+            return 'Enter your message...';
+          default:
+            return 'Enter your message...';
+        }
+      },
+
+      /* FormKit */
+
+
+      /* Old Form Plugin
       async submitForm() {
         const response = await fetch("https://api.web3forms.com/submit", {
           method: "POST",
@@ -64,10 +101,10 @@ export default {
         const result = await response.json();
         if (result.success) {
           console.log(result);
-        }
-      },
-    },
-  };
+        } 
+      }, */
+    }
+}
   </script>
 
 <style>
@@ -141,6 +178,11 @@ form message, textarea {
   width: 95%;
   border-radius: 6px;
   margin-bottom: 10px;
+}
+
+.input-style::placeholder {
+  font-style: italic;
+  line-height: 1.6;
 }
 
 form button {

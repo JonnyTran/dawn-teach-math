18cf00ab2c9b4f10e6ef0b1470af5e6798f5993b
Dawn Tran <kdoantran@gmail.com>
2023-07-11 17:36:33 -0500

    course page general FAQ

--- src/views/CourseView.vue
+++ src/views/CourseView.vue
@@ -29,18 +29,77 @@ const teacherStore = useTeacherStore();
         <a :href="link.url" target="_blank">{{ link.title }}</a>
       </li>
     </ul>
-
-    <h2>FAQ</h2>
-    <ul>
-      <li v-for="faq in course.faq" :key="faq.id">
-        <p><strong>{{ faq.question }}</strong></p>
-        <p>{{ faq.answer }}</p>
-      </li>
-    </ul>
+    <br>
+    <faqhead><h1>FAQ</h1></faqhead>
+    <faq>
+    <Accordion>
+    <accordion-panel>
+      <accordion-header>What are your tutoring hours?</accordion-header>
+      <accordion-content>
+        <div>
+          <p>
+            <li>After school Monday/Wednesday 4:20-5:00 PM</li>
+            <li>Morning by teacher availability</li>
+            <li>Flex self-schedule on Edficiency</li></p>
+            Note that the above hours are subject to change. 
+            Contact Ms. Tran for any scheduling inquiries.
+        </div>
+      </accordion-content>
+    </accordion-panel>
+    <accordion-panel>
+      <accordion-header>How do I turn in assignments?</accordion-header>
+      <accordion-content>
+        <div>
+          <p>All assignments will be posted and submitted on Schoology. 
+            Students should see a list of assignments listed on the right side of the course page. 
+            Click on each assignment for instructions, due dates, and turning in work. 
+            <br>Students will not turn in any paper copies unless given notice otherwise. 
+            Name and work must be clearly shown to be able to receive full credit. 
+            Check Schoology or contact Ms. Tran for instructions on specific assignments.</p>
+        </div>
+      </accordion-content>
+    </accordion-panel>
+    <accordion-panel>
+      <accordion-header>What is the late-work policy?</accordion-header>
+      <accordion-content>
+        <div>
+          <p>Students are responsible for checking Schoology, making up missed assignments, and communicating with Ms. Tran about grade updates (see contact page). 
+          Late work may be graded up to a 70 and can be submitted until the 9-week grading period is over.
+          Late work may be eligible for full credit only if the student has been absent and has discussed with the teacher about making up missed assignments.</p>
+          <br>In extenuating circumstances, please have a parent or guardian contact Ms. Tran.
+        </div>
+      </accordion-content>
+    </accordion-panel>
+    <accordion-panel>
+      <accordion-header>How do I make up a missed test or quiz?</accordion-header>
+      <accordion-content>
+        <div>
+          <p>Students are responsible for being up-to-date on upcoming quizzes and tests dates. 
+          If a student is absent on the day of a quiz or test, they must contact Ms. Tran to schedule a make-up time and complete 
+          Late work may be eligible for full credit only if the student has been absent and has discussed with the teacher about making up missed assignments.</p>
+        </div>
+      </accordion-content>
+    </accordion-panel>
+    <accordion-panel>
+      <accordion-header>What is the retest policy?</accordion-header>
+      <accordion-content>
+        <div>
+          <p>
+            Students are responsible for making up any tests up to a 70 within the grading period. Contact Ms. Tran a day in advance to set up a retest:
+            <li>After school Monday/Wednesday 4:20-5:45 PM</li>
+            <li>Morning by teacher availability</li>
+            <li>Flex self-schedule on Edficiency</li>
+            Note that the above hours are subject to change and that a retest may not be guaranteed without prior notice. Contact Ms. Tran for any scheduling inquiries.</p>
+        </div>
+      </accordion-content>
+    </accordion-panel>
+  </Accordion>
+  </faq>
   </div>
 </template>
 
 <script>
+import { Accordion, AccordionPanel, AccordionHeader, AccordionContent } from 'flowbite-vue'
 export default {
   name: 'CoursePage',
   data() {

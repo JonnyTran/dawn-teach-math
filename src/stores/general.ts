8fe329f9b0b35bb9c07c9fc05828642ef37647c2
JonnyTran <nhat.tran@mavs.uta.edu>
2023-08-27 11:29:38 -0500

    added django

--- src/stores/general.ts
+++ src/stores/general.ts
@@ -1,6 +1,16 @@
 import { defineStore } from 'pinia'
 import axios from 'axios';
+import Cookies from 'js-cookie';
 import addOAuthInterceptor from 'axios-oauth-1.0a'
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+
+const csrftoken = Cookies.get('csrftoken');
+
+if (csrftoken) {
+  axios.defaults.headers.common['X-CSRFToken'] = csrftoken;
+}
 
 const CONSUMERKEY = import.meta.env.VITE_CONSUMERKEY;
 const CONSUMERSECRET = import.meta.env.VITE_CONSUMERSECRET;

71df0e50ea16e5c8cdf7131de7f9d02d7af10fda
Jonny Tran <nhat.tran@mavs.uta.edu>
2023-09-06 21:01:09 -0500

    try

--- api/index.py
+++ api/index.py
@@ -11,8 +11,6 @@ load_dotenv(find_dotenv())
 
 app = FastAPI()
 
-asyncio.set_event_loop_policy(asyncio.get_event_loop_policy())
-
 try:
     base_url = os.environ.get('API_BASE_URL', default="https://api.schoology.com/v1/")
     if base_url is None or base_url == '':
@@ -28,36 +26,37 @@ except KeyError as ke:
 
 limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
 timeout = httpx.Timeout(timeout=5.0, read=15.0)
+client = httpx.AsyncClient(limits=limits, timeout=timeout, auth=oauth)
+asyncio.set_event_loop_policy(asyncio.get_event_loop_policy())
 
 @app.on_event("shutdown")
 async def shutdown_event():
     print("shutting down...")
     await client.aclose()
 
+
 @app.get("/api/{path:path}")
 async def proxy_schoology_api(path: str):
     """
     Proxies GET requests to the API_BASE_URL with OAuth1 authentication.
     """
-    async with httpx.AsyncClient(limits=limits, timeout=timeout, auth=oauth) as client:
+    url = os.path.join(base_url, path.replace('/api', ''))
 
-        url = os.path.join(base_url, path.replace('/api', ''))
+    try:
+        response = await client.get(url, timeout=5)
 
-        try:
-            response = await client.get(url)
-
-            response.raise_for_status()
-        
-        except httpx.ConnectTimeout:
-            return JSONResponse(content={"error": "Connection timeout"}, status_code=408)
-        except httpx.ReadTimeout:
-            return JSONResponse(content={"error": "Read timeout"}, status_code=408)
-        except httpx.HTTPError:
-            return JSONResponse(content={"error": "HTTP error"}, status_code=500)
-        except Exception as e:
-            print(e)
-            return JSONResponse(content={"error": "Unknown error"}, status_code=500)
-        
-        return JSONResponse(content=response.json(), status_code=response.status_code)
+        response.raise_for_status()
+    
+    except httpx.ConnectTimeout:
+        return JSONResponse(content={"error": "Connection timeout"}, status_code=408)
+    except httpx.ReadTimeout:
+        return JSONResponse(content={"error": "Read timeout"}, status_code=408)
+    except httpx.HTTPError:
+        return JSONResponse(content={"error": "HTTP error"}, status_code=500)
+    except Exception as e:
+        print(e)
+        return JSONResponse(content={"error": "Unknown error"}, status_code=500)
+    
+    return JSONResponse(content=response.json(), status_code=response.status_code)
 
     

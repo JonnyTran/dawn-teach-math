872d128d7b8805a58bc2bc3d0af6b48bced26a3c
Jonny Tran <nhat.tran@mavs.uta.edu>
2023-09-20 11:35:34 -0700

    test FAQ

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -66,7 +66,7 @@ onMounted(() => {
   <section v-if="faqData">
     <h3 
       class="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-5xl dark:text-white"
-      id="componentId"
+      :id="componentId"
     >
       General FAQ
     </h3>
